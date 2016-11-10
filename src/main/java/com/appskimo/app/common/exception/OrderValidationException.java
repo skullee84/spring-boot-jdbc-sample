fc68a1721ad5ef09de86295e050ea6818b4f75c6
dominic.lee <dominic.lee@kakaocorp.com>
2016-11-10 15:23:23 +0900

    no message

--- src/main/java/com/appskimo/app/common/exception/OrderValidationException.java
+++ src/main/java/com/appskimo/app/common/exception/OrderValidationException.java
@@ -4,6 +4,7 @@ package com.appskimo.app.common.exception;
  * Created by dominic on 2016-10-22.
  */
 public class OrderValidationException extends RuntimeException {
+    private static final long serialVersionUID = -1971031804368193638L;
 
     public OrderValidationException(String message) {
         super(message);
